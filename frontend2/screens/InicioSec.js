84e6d9de8b84cf0c2afdcf52b3bbb58b6eca1b2c
Fabricio piccininno <piccininno.fabriciol@gmail.com>
2025-06-22 03:59:26 -0300

    Update InicioSec.js

--- frontend2/screens/InicioSec.js
+++ frontend2/screens/InicioSec.js
@@ -15,7 +15,7 @@ export default function InicioSec({ navigation }) {
     }
 
     try {
-      const response = await fetch("http://192.168.0.246:3001/api/login", {
+      const response = await fetch("http://localhost:3001/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
